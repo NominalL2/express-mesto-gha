3b223e2fd41b065d246492d2116892465eba3476
unknown <german280105@mail.ru>
2023-04-02 15:57:01 +0300

    добавил cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const NotFoundError = require('./errors/NotFoundError');
 const { login, createUser } = require('./controllers/users');
@@ -23,6 +24,8 @@ const router = express.Router();
 const { PORT = '3000' } = process.env;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
